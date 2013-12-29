6d0f0a4de27ca69460775482f8dae99811ec94bd
fixnix <fixnix@fixnix-virtual-machine.(none)>
2013-12-29 13:54:33 +0530

    first commit

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,5 @@
 WickedPdf.config = {
-    :wkhtmltopdf => '/opt/wkhtmltopdf',
+    :wkhtmltopdf => "#{RAILS_ROOT}/pdfbin/wkhtmltopdf-i386",
     :layout => "pdf.html",
     :margin => {    :top=> 40,
                     :bottom => 20,
---
+++ s/config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_s_session',
+  :secret      => '8d40bcbe45451e54d09174936b9a7709abeb454e128fccdfe06f44ba4f90291874fdee6e01a5069f2508633f57604e8c693478e34979bd1dfeab52353ef6310d'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
